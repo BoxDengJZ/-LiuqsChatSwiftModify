cf51f1abe3eabd54dfb6ab08ad75601ad158b366
邓江洲 <dengjiangzhouReal@gmail.com>
2022-04-24 14:50:05 +0800

    swift, paraseTemplateFile

---
+++ CoreTextCoreSwift/CoreTextCoreSwift/CoreTextCoreSwift-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import "CTDisplayView.h"
+
+#import "CTFrameParserConfig.h"
+
+#import "CoreTextData.h"
+
+#import "CTFrameParser.h"
---
+++ CoreTextCoreSwift/CoreTextCoreSwift/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CoreTextCoreSwift
+//
+//  Created by Jz D on 2022/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let displayView = CTDisplayView(frame: CGRect(x: 10, y: 100, width: 200, height: 200))
+        displayView.backgroundColor = UIColor.yellow
+        view.addSubview(displayView)
+        let config = CTFrameParserConfig()
+        config.width = 300
+        if let path = Bundle.main.path(forResource: "content", ofType: "json"){
+            let data = CTFrameParser.paraseTemplateFile(path, config: config)
+            displayView.data = data;
+            displayView.frame = CGRect(x: 10, y: 100, width: 300, height: data.height)
+        }
+        
+    }
+
+
+}
+
