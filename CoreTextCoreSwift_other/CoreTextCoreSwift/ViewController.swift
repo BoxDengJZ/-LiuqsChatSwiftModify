3224a05f2d7c1197030a2b4343961bf5d1a18d2b
邓江洲 <dengjiangzhouReal@gmail.com>
2022-04-24 16:32:25 +0800

    code adjust

--- CoreTextCoreSwift_other/CoreTextCoreSwift/ViewController.swift
+++ CoreTextCoreSwift_other/CoreTextCoreSwift/ViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var displayView = CTDisplayView(frame: CGRect(x: 10, y: 100, width: 200, height: 200))
+    
+    let config = CTFrameParserConfig()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let displayView = CTDisplayView(frame: CGRect(x: 10, y: 100, width: 200, height: 200))
+        
         displayView.backgroundColor = UIColor.yellow
         view.addSubview(displayView)
-        let config = CTFrameParserConfig()
+        
         config.width = 300
         if let path = Bundle.main.path(forResource: "content", ofType: "json"){
             let data = CTFrameParser.parse(TemplateFile: path, with: config)
@@ -25,5 +29,12 @@ class ViewController: UIViewController {
     }
 
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let path = Bundle.main.path(forResource: "666", ofType: "json"){
+            let data = CTFrameParser.parse(TemplateFile: path, with: config)
+            displayView.data = data;
+            displayView.frame = CGRect(x: 10, y: 100, width: 300, height: data.height)
+        }
+    }
 }
 
